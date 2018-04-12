6844b3e75c0920c4fcb7eb08b95de61c56648928
Robzah <rob@cabalist.co.uk>
2018-04-12 19:21:08 +0100

    tweaks

--- linuxProvisioning.sh
+++ linuxProvisioning.sh
@@ -6,12 +6,18 @@ echo "***installing tomcat***"
 sudo apt-get install -y tomcat8 tomcat8-admin &>/dev/null
 sudo cp /vagrant/scripts/tomcat-users.xml /var/lib/tomcat8/conf/tomcat-users.xml
 sudo systemctl restart tomcat8 &>/dev/null
+echo
 echo "***Status***"
 echo
-curl -I -s -L 192.168.0.40:8080 | grep "HTTP/1.1"
+responce=$(curl -so /dev/null -w '%{response_code}' 192.168.0.40:8080) 
+if [ $responce -eq 200 ]; then
+	echo "Tomcat status (200) ok"
+else
+	echo "An error occured during tomcat instalation"
+	exit -1
+fi
 echo
-echo "******complete******"
-echo
+echo "******installation complete******"
 echo "***tomcat manager available at***"
 echo "http://192.168.0.40:8080/manager/html"
 echo "Username = admin  password = admin"
