684916c8864c779542812f95fe9d6a32563878db
Robzah <rob@cabalist.co.uk>
2018-04-15 19:47:08 +0100

    tweaks2

--- linuxProvisioning.sh
+++ linuxProvisioning.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
-echo "***Preparing to install tomcat***"
-apt-get update &>/dev/null
-echo
-echo "***installing tomcat***"
-sudo apt-get install -y tomcat8 tomcat8-admin &>/dev/null
-sudo cp /vagrant/scripts/tomcat-users.xml /var/lib/tomcat8/conf/tomcat-users.xml
-sudo systemctl restart tomcat8 &>/dev/null
-echo
-echo "***Status***"
-echo
-responce=$(curl -so /dev/null -w '%{response_code}' 192.168.0.40:8080) 
-if [ $responce -eq 200 ]; then
-	echo "Tomcat status (200) ok"
-else
-	echo "An error occured during tomcat instalation"
-	exit -1
-fi
-echo
-echo "******installation complete******"
-echo "***tomcat manager available at***"
-echo "http://192.168.0.40:8080/manager/html"
-echo "Username = admin  password = admin"
+
+LOGFILE="/vagrant/tomcatinstall.log"
+TOMCAT_URL="192.168.0.40:8080"
+APPS=(tomcat8 tomcat8-admin)
+SCRIPT="/vagrant/scripts/tomcat-users.xml"
+TDIR="/var/lib/tomcat8/conf/"
+
+install_apps(){
+    echo "Installing tomcat 8 please wait..."
+    apt-get update &>/dev/null
+    sudo apt-get install -y ${APPS[*]} > "$LOGFILE" 2>&1
+    cp "$SCRIPT" "$TDIR" >> "$LOGFILE" 2>&1
+    sudo systemctl restart tomcat8 >> "$LOGFILE" 2>&1
+}
+
+test_tomcat(){
+    response=$(curl -sSo /dev/null 2>> "$LOGFILE" -w '%{response_code}' "$TOMCAT_URL")
+    if [ $response -eq 200 ]; then
+        echo "installation complete..."
+	      echo  "tomcat manager available at..."
+        echo "http://192.168.0.40:8080/manager/html"
+        echo "Username: admin  password: admin"
+    else
+	      echo "An error occurred during tomcat installation"
+        echo "Please check" "$LOGFILE" 
+	      exit -1
+    fi
+}
+
+install_apps
+test_tomcat
